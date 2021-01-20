4be078d96abe5426c58ae6dadfa061c524ed8d81
Aleksandr <vonabahs.a@gmail.com>
2021-01-20 16:52:27 +0300

    Add Day1 solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int num = 1;
+        int k = 5;
+        while (num <= 9) {
+            System.out.println(num + " х " + k + " = " + num * k);
+            num++;
+        }
     }
 }
