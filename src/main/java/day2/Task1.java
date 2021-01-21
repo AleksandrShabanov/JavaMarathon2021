a4c7984c93996faf802ec6a245df7283eaf63b14
Aleksandr <vonabahs.a@gmail.com>
2021-01-21 22:31:42 +0300

    Add Day2 solutions

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        if (x <= 0) {
+            System.out.println("Ошибка ввода");
+        } else if (x >= 1 && x <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (x >= 5 && x < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else {
+            System.out.println("Многоэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,29 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+
+        while (a < b) {
+            int counter = a;
+            counter++;
+
+            if (counter % 5 == 0 && counter % 10 != 0) {
+                System.out.print(counter + " ");
+            }
+
+            a++;
+        }
+
 
     }
 }
