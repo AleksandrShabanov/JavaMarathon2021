ff333cb6d6130ed8e28c62270ad7cb4b792fe7d5
Aleksandr <vonabahs.a@gmail.com>
2021-02-07 14:13:16 +0300

    Add Day17 solutions

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,25 @@
 package day17;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece whitePawn = ChessPiece.PAWN_WHITE;
+        ChessPiece blackRook = ChessPiece.ROOK_BLACK;
+
+        String[] arrayOfChessPieces = new String[8];
+
+        for (int i = 0; i < arrayOfChessPieces.length; i++){
+            if (i < 4)
+                arrayOfChessPieces[i] = whitePawn.getRepresentation();
+
+            else
+                arrayOfChessPieces[i] = blackRook.getRepresentation();
+        }
+
+        for (int j = 0; j < arrayOfChessPieces.length; j++){
+            System.out.print(arrayOfChessPieces[j] + " ");
+        }
 
     }
 }
